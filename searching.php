ee55bf1659f289d01a078e7a3001c6156e750082
ict_intern <ict_intern@puncakniaga.com.my>
2025-01-06 11:44:52 +0800

    Search Change

--- searching.php
+++ searching.php
@@ -8,7 +8,7 @@ include 'header.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Search & Filters</title>
+    <title>Search & Filters right</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <style>
